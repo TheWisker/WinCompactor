cd4aeb53608c4767a7dceacbe5473b9603479fd4
TheWisker <stefanobia227@gmail.com>
2023-04-24 17:52:41 +0200

    First version

--- main.bat
+++ main.bat
@@ -20,15 +20,8 @@ cleanmgr /verylowdisk /d
 del C:\Windows\prefetch\*.*/s/q
 Dism.exe /online /Cleanup-Image /StartComponentCleanup /ResetBase
 
-
-
 echo "Running debloater!"
 powershell -Command "& {.debloater\Windows10SysPrepDebloater.ps1 -Sysprep -Debloat -Privacy}"
-
-echo "Following packages should be removed:"
-powershell -Command "& {Get-AppXProvisionedPackage -Online | Select PackageName}"
-echo "Remove by running: Remove-AppXProvisionedPackage -Online -PackageName {package-name}"
-pause
 
 echo "Disabling some services!"
 regsvr32.exe /u hnetmon.dll
@@ -39,11 +32,16 @@ netsh winsock reset
 echo "Flushing DNS cache!"
 ipconfig /flushdns
 
-echo "Defragmenting drive %_disk%:\!"
-if /i "%_defrg%" == "y" defrag %_disk%: /o
+echo "Following packages should be removed:"
+powershell -Command "& {Get-AppXProvisionedPackage -Online | Select PackageName}"
+echo "Remove by running: Remove-AppXProvisionedPackage -Online -PackageName {package-name}"
+pause
 
 echo "Compacting OS!"
 Compact.exe /CompactOS:always
+
+echo "Defragmenting drive %_disk%:\!"
+if /i "%_defrg%" == "y" defrag %_disk%: /o
 
 echo "Scanning for system problems!"
 sfc /scannow
@@ -51,4 +49,8 @@ sfc /scannow
 echo "This are the remaining programs:"
 wmic product get name
 
+echo "Opening optimizer!"
+optimizer/optimizer.exe
+
 echo "All done!"
+pause
